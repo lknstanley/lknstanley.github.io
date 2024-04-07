556817592f5bbfffcb35867cfc4135db8b8e0c07
Stanley Lai <stanley.lai.104@gmail.com>
2024-04-07 18:21:21 +0800

    Remove unoptimized iamge

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
   output: "export",
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
