e2f05cb3b83ee2425c42f4b1207c02ea323afae5
Stanley Lai <stanley.lai.104@gmail.com>
2024-04-07 18:23:50 +0800

    Add unoptimised image for static export

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   output: "export",
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
